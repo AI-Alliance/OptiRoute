1c82517ffb5bc1b140bcf3f6b471db5dc2a02a9c
Nick <zenevichnikita2001@gmail.com>
2023-11-12 20:38:23 +0100

    demand problem fixed

--- backend/logic/algorithms/implementations/GreedyAlgorithm.py
+++ backend/logic/algorithms/implementations/GreedyAlgorithm.py
@@ -11,7 +11,13 @@ class GreedyAlgorithm(Algorithm):
         clients = task.places[1:]
         vehicles: list[Vehicle] = task.vehicles
         unvisited_clients: list[Place] = clients.copy()
+
+        vehicles_id_to_current_capacity = {}
+        for vehicle in vehicles:
+            vehicles_id_to_current_capacity[vehicle.vehicle_id] = vehicle.capacity
+
         vehicles_id_to_places_dict = {}
+
         # starting from depot
         for vehicle in vehicles:
             vehicles_id_to_places_dict[vehicle.vehicle_id] = [depot]
@@ -22,14 +28,17 @@ class GreedyAlgorithm(Algorithm):
             for vehicle in vehicles:
                 last_place_visited_by_vehicle = vehicles_id_to_places_dict[vehicle.vehicle_id][-1]
                 for client in unvisited_clients:
-                    possible_distance = task.distance_matrix[last_place_visited_by_vehicle.place_index][client.place_index]
-                    if vehicle.capacity >= client.demand and possible_distance < shortest_distance:
+                    possible_distance = task.distance_matrix[last_place_visited_by_vehicle.place_index][
+                        client.place_index]
+                    if vehicles_id_to_current_capacity[
+                        vehicle.vehicle_id] >= client.demand and possible_distance < shortest_distance:
                         best_pair = (vehicle, client)
                         shortest_distance = possible_distance
             if best_pair[0] is not None:
                 best_vehicle: Vehicle = best_pair[0]
                 best_client: Place = best_pair[1]
                 vehicles_id_to_places_dict[best_vehicle.vehicle_id].append(best_client)
+                vehicles_id_to_current_capacity[best_vehicle.vehicle_id] -= best_client.demand
                 unvisited_clients.remove(best_client)
             else:
                 break
