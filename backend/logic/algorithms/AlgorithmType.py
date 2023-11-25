ce8737f9a3d6c54bfb89687b3fe66da6ea78d2c2
Nick <zenevichnikita2001@gmail.com>
2023-11-25 12:22:35 +0100

    fixed float error

--- backend/logic/algorithms/AlgorithmType.py
+++ backend/logic/algorithms/AlgorithmType.py
@@ -3,3 +3,4 @@ from enum import Enum
 class AlgorithmType(Enum):
     TABU = 1
     GREEDY = 2
+    GOOGLE = 3
---
+++ backend/logic/algorithms/implementations/GoogleAlgorithm.py
@@ -0,0 +1,100 @@
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+from logic import Task
+from logic.Solution import Solution
+from logic.algorithms.Algorithm import Algorithm
+from logic.models import Place
+from logic.models.Vehicle import Vehicle
+
+
+class GoogleAlgorithm(Algorithm):
+    def solve(self, task: Task) -> Solution:
+        """Entry point of the program."""
+        # Instantiate the data problem.
+        data = self.create_data_model(task.distance_matrix)
+
+        # Create the routing index manager.
+        manager = pywrapcp.RoutingIndexManager(
+            len(data["distance_matrix"]), data["num_vehicles"], data["depot"]
+        )
+
+        # Create Routing Model.
+        routing = pywrapcp.RoutingModel(manager)
+
+        # Create and register a transit callback.
+        def distance_callback(from_index, to_index):
+            """Returns the distance between the two nodes."""
+            # Convert from routing variable Index to distance matrix NodeIndex.
+            from_node = manager.IndexToNode(from_index)
+            to_node = manager.IndexToNode(to_index)
+            return data["distance_matrix"][from_node][to_node]
+
+        transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+        # Define cost of each arc.
+        routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+        # Add Distance constraint.
+        dimension_name = "Distance"
+        routing.AddDimension(
+            transit_callback_index,
+            0,  # no slack
+            3000,  # vehicle maximum travel distance
+            True,  # start cumul to zero
+            dimension_name,
+        )
+        distance_dimension = routing.GetDimensionOrDie(dimension_name)
+        distance_dimension.SetGlobalSpanCostCoefficient(100)
+
+        # Setting first solution heuristic.
+        search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+        search_parameters.first_solution_strategy = (
+            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+        )
+
+        # Solve the problem.
+        solution = routing.SolveWithParameters(search_parameters)
+
+        # Print solution on console.
+        if solution:
+            self.print_solution(data, manager, routing, solution)
+        else:
+            print("No solution found !")
+
+        vehicles_id_to_places_dict = {}
+        return Solution(task, vehicles_id_to_places_dict)
+
+
+    def create_data_model(self, distance_matrix):
+        """Stores the data for the problem."""
+        data = {}
+        data["distance_matrix"] = (distance_matrix.astype(int)).tolist()
+
+        #distance_matrix.tolist()
+        print(distance_matrix.tolist())
+        data["num_vehicles"] = 2
+        data["depot"] = 0
+        return data
+
+    def print_solution(self, data, manager, routing, solution):
+        """Prints solution on console."""
+        print(f"Objective: {solution.ObjectiveValue()}")
+        max_route_distance = 0
+        for vehicle_id in range(data["num_vehicles"]):
+            index = routing.Start(vehicle_id)
+            plan_output = f"Route for vehicle {vehicle_id}:\n"
+            route_distance = 0
+            while not routing.IsEnd(index):
+                plan_output += f" {manager.IndexToNode(index)} -> "
+                previous_index = index
+                index = solution.Value(routing.NextVar(index))
+                route_distance += routing.GetArcCostForVehicle(
+                    previous_index, index, vehicle_id
+                )
+            plan_output += f"{manager.IndexToNode(index)}\n"
+            plan_output += f"Distance of the route: {route_distance}m\n"
+            print(plan_output)
+            max_route_distance = max(route_distance, max_route_distance)
+        print(f"Maximum of the route distances: {max_route_distance}m")
+
